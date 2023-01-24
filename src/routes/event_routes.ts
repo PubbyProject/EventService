29ddebf60b15839cbafe0166c1f5a3e95cd316bd
calpet <calinpeters98@hotmail.nl>
2023-01-24 22:48:37 +0100

    Changed routes

--- src/routes/event_routes.ts
+++ src/routes/event_routes.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import controller from '../controllers/event_controller';
 const router = express.Router();
 
-router.get('/events/all', controller.getEvents);
+router.get('/events', controller.getEvents);
 router.get('/events/:id', controller.getEventById);
-router.post('/events/create', controller.addEvent);
+router.post('/events', controller.addEvent);
 
 export = router;
