0523290dea6a9cae12f060680cbbb6f78d5966b8
Calin Peters <calinpeters98@hotmail.nl>
2023-02-03 15:42:27 +0100

    Implemented algorithm to keep track of incoming & processed messages to prevent the producer from infinitely producing new messages.

--- src/services/messaging/rabbitmq_service.ts
+++ src/services/messaging/rabbitmq_service.ts
@@ -33,14 +33,33 @@ export default class RabbitMQService {
   }
 
   public async ConsumeMessage(connection: Connection) {
+    // Keep track of how many messages are incoming:
+    let incomingMessagesCount: number = 0;
     const consumer = connection.createConsumer({
-        queue: this.queueName,
-        qos: {prefetchCount: 2}
+        queue: 'fetch-organizer-events-request-queue',
+        qos: {prefetchCount: 2},
+        exchanges: [{exchange: 'organizer-events-exchange', type: 'topic', autoDelete: true, durable: true}],
+        queueBindings: [
+          {exchange: 'organizer-events-exchange', routingKey: 'events.fetch'}
+        ]
     },
     async (message) => {
-        let content = message.body as RequestMessage;
+      let content = message.body as RequestMessage;
+      console.log(`Incoming message: ${content.organizerId}`)
+      // Keep track of how many messages have been processed:
+      let processedMessagesCount: number = 0;
+      incomingMessagesCount++;
+      console.log(`Incoming: ${incomingMessagesCount}`)
+      /* Only send messages to the producer if the number of processed messages is lower or equal to the number of incoming messages to prevent the consumer from 
+         infinitely processing messages and overloading the message broker (we cannot close the connection either or the consumer won't consume, i hate this library): */
+      if (processedMessagesCount !== incomingMessagesCount) {
         const events = await this.eventService.fetchEventsByOrganizerId(content.organizerId);
         this.ProduceMessage(connection, events);
+        processedMessagesCount++;
+        console.log(`Processed: ${processedMessagesCount}`)
+      }
+
+      await consumer.close();
     });
 
     consumer.on('error', (err) => {
@@ -52,7 +71,7 @@ export default class RabbitMQService {
       const producer = connection.createPublisher({
         confirm: true,
         maxAttempts: 2,
-        exchanges: [{exchange: 'organizer-events-exchange', type: 'topic', autoDelete: true, durable: true}]
+        exchanges: [{exchange: 'organizer-events-exchange', type: 'topic', autoDelete: true, durable: true}],
       });
 
       await producer.publish({exchange: 'organizer-events-exchange', routingKey: 'events.result'}, message);
